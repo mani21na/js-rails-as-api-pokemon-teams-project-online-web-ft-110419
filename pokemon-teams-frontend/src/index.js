70b5b25896ea532214d2b74e588593236c9ebe79
Hanna Lee <mani21na@gmail.com>
2020-03-12 23:27:37 +0000

    Automatically backed up by Learn

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,75 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadTeams();
+});
+
+function loadTeams() {
+
+    const teamData = TRAINER_URL
+
+    fetch(teamData)
+        .then(res => res.json())
+        .then(results => {
+            results.data.forEach(trainer => addCard(trainer))
+            results.included.forEach(pokemon => listPokemon(pokemon))
+            console.log(results)
+        });
+}
+
+//Watch video, link to website, use debugger on fetch, trainer
+
+function addCard(trainer) {
+    let card = document.createElement('div');
+    let body = document.querySelector('body');
+    let pokemonList = document.createElement('ul');
+    let addButton = document.createElement('button');
+
+    pokemonList.id = 'pokemon-list-' + trainer.id;
+    addButton.id = 'add-button';
+    addButton.innerText = 'Add Pokemon'
+    card.className = "card";
+
+    trainerName = document.createElement('h1');
+    trainerName.innerHTML = trainer.attributes.name;
+
+    body.appendChild(card)
+    card.appendChild(trainerName)
+    card.appendChild(addButton)
+    card.appendChild(pokemonList)
+
+    // if trainer.relationships.pokemon.data.id
+
+    // trainer.relationships.pokemon.data.forEach(pokemon => listPokemon(pokemon))
+
+
+    document.getElementById("add-button").addEventListener("click", addPokemon);
+}
+
+function listPokemon(pokemon) {
+    let pokemonList = document.querySelector(`#pokemon-list-${pokemon.relationships.trainer.data.id}`);
+    let listItem = document.createElement('li');
+    let releaseButton = document.createElement('button');
+    releaseButton.className = 'release';
+    releaseButton.innerText = 'Release';
+
+    listItem.innerHTML = pokemon.attributes.nickname + ' (' + pokemon.attributes.species + ')'
+
+    listItem.appendChild(releaseButton)
+
+    pokemonList.appendChild(listItem)
+
+    releaseButton.addEventListener("click", releasePokemon);
+
+}
+
+function addPokemon() {
+
+
+}
+
+function releasePokemon() {
+
+}
